a31373ed9205eedd36c6de0e042a7739638da94b
Zefei Guo <guozefei@gmail.com>
2017-05-18 23:07:07 +0800

    add step 4

---
+++ step4/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }     from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule }     from './heroes/heroes.module';
+import { CrisisCenterModule }   from './crisis-center/crisis-center.module';
+import { PageNotFoundComponent } from './not-found.component';
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HeroesModule,
+    CrisisCenterModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export default class AppModule { }
